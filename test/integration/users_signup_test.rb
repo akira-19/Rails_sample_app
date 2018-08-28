be0f040909a67d370660ad6570402cc7d1c66700
akira <akira@MBP.local>
2018-08-28 16:04:19 -0700

    finish chap8

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -6,7 +6,7 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     get signup_path
 		assert_select 'form[action="/signup"]'
     assert_no_difference 'User.count' do
-      post signup_path, params: { user: { name:  "",
+      post users_path, params: { user: { name:  "",
                                          email: "user@invalid",
                                          password:              "foo",
                                          password_confirmation: "bar" } }
@@ -27,7 +27,7 @@ test "valid signup information" do
 	 end
 	 follow_redirect!
 	 assert_template 'users/show'
-	 assert_not flash.empty?
+	 assert is_logged_in?
  end
 
 end
