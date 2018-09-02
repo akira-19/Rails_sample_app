5304405b97b2130910944b2cee2fd223fb6e90d4
akira <akira@MBP.local>
2018-09-02 15:33:02 -0700

    finish chap10

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -2,13 +2,34 @@ require 'test_helper'
 
 class SiteLayoutTest < ActionDispatch::IntegrationTest
 
-	test "layout links" do
+	def setup
+		@user = users(:michael)
+	end
+
+	test "layout links by non user" do
 		get root_path
 		assert_template 'static_pages/home'
     assert_select "a[href=?]", root_path, count: 2
     assert_select "a[href=?]", help_path
     assert_select "a[href=?]", about_path
     assert_select "a[href=?]", contact_path
+		assert_select "a[href=?]", login_path
+		get contact_path
+    assert_select "title", full_title("Contact")
+		get signup_path
+    assert_select "title", full_title("Sign up")
+	end
+
+	test "layout links by a user" do
+		log_in_as(@user)
+		get root_path
+		assert_template 'static_pages/home'
+    assert_select "a[href=?]", root_path, count: 2
+    assert_select "a[href=?]", help_path
+    assert_select "a[href=?]", about_path
+    assert_select "a[href=?]", contact_path
+		assert_select "a[href=?]", user_path(@user)
+		assert_select "a[href=?]", logout_path
 		get contact_path
     assert_select "title", full_title("Contact")
 		get signup_path
